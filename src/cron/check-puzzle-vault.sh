8b47f81f25ca9171ef328648ae10b5643e024ddf
Oguz Han Asnaz <github@lostgeek.de>
2023-10-06 06:47:50 +0200

    Added cron script to remember filling up daily puzzles

---
+++ src/cron/check-puzzle-vault.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+puzzle_folder="~/devel/gordianbla/daily-puzzles"
+
+current=$(curl -s https://gordianbla.de/current_daily_puzzle.php | sed 's/^0*//')
+current=${current%%.*}
+
+existing=$(ls -1 $puzzle_folder/*.svg.gz | tail -n 1 | xargs -n 1 basename | sed 's/^0*//')
+existing=${existing%%.*}
+
+days_left=$((existing - current))
+
+if (( days_left < 14 )); then
+    echo $days_left "days of puzzles left!"
+fi
